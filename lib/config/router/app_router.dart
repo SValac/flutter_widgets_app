03303ed742d1fdcba25cb9d4a1e7385efd5f6c12
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-08-30 20:36:06 -0600

    refactor: consolidate screen imports into a single screens.dart file

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,7 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
