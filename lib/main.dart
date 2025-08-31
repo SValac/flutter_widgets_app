a3d9158ffec0d0dc1b5bed3ee748621feb5c4a0e
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-08-30 20:24:32 -0600

    fix: correct import path for ButtonsScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_creen.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
