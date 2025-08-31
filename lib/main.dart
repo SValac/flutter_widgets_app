36cf0439615d842601ca256e35d5121f8ca2e995
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-08-30 20:46:50 -0600

    feat: update navigation in HomeScreen to use GoRouter and remove legacy Navigator code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 
 import 'config/theme/app_theme.dart';
 
@@ -14,16 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
+
       // home: const HomeScreen(), When using initialRoute, don't define a home property.
-      initialRoute: '/',
-      routes: {
-        '/': (contex) => const HomeScreen(),
-        '/buttons': (contex) => const ButtonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
     );
   }
 }
