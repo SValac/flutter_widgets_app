97be4e06145f692ade9889a3937c1e030658152c
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-08-30 20:35:45 -0600

    feat: add GoRouter configuration with initial routes for Home, Buttons, and Cards screens

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
+    GoRoute(path: '/buttons', builder: (contex, state) => ButtonsScreen()),
+    GoRoute(path: '/cards', builder: (contex, state) => CardsScreen()),
+  ],
+);
