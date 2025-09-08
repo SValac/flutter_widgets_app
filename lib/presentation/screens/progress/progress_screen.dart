aeef2289b728cabfeb7d89a414f0fa0b5c53faf8
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-08 01:53:15 -0600

    feat: implement ProgressScreen with circular and linear progress indicators

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,63 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Progress Indicators')),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(height: 30),
+          Text('Circular Progress Indicator'),
+          SizedBox(height: 30),
+          CircularProgressIndicator(backgroundColor: Colors.black45),
+
+          SizedBox(height: 30),
+          Text('Progress Indicator Controlado Lineal y Circular'),
+          SizedBox(height: 30),
+          _ControlledPRogressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledPRogressIndicator extends StatelessWidget {
+  const _ControlledPRogressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value <= 100),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12,
+              ),
+              SizedBox(width: 20),
+              Expanded(child: LinearProgressIndicator(value: progressValue)),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
