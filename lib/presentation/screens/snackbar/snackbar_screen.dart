fb0a2c97af284d99b6e8c9ae0ef21c2571b3441d
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-08 12:57:40 -0600

    feat: implement SnackbarScreen with custom snackbar display functionality

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,27 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('hola Mundo'),
+      action: SnackBarAction(label: 'ok', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Snackbars y Dialogs')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Mostrar Snackbar'),
+        icon: Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
