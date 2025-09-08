7c35fe350a84e6a6cf42652bf8e7895aed459f66
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-08 01:53:02 -0600

    feat: add ProgressIndicators menu item to app menu

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -27,4 +27,10 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card,
   ),
+  MenuItem(
+    title: 'ProgressIndicators',
+    subtitle: 'Generales y Controlados',
+    link: '/progress',
+    icon: Icons.refresh_outlined,
+  ),
 ];
