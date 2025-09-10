970a44524d761687fe9f4772dc6b63dcf98017f4
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-09 18:38:59 -0600

    feat: update app title to 'Flitter Widgets'

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Flitter Widgets',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 7).getTheme(),
