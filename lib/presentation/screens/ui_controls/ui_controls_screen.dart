133587d7b701614d50ef41207a07766e758d17dc
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-15 02:18:46 -0600

    feat: implement UiControlsScreen with developer mode toggle

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,37 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Ui Controls')),
+      body: _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool developerMode = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: Text('Developer Mode'),
+          subtitle: Text('Controles adicionales'),
+          value: developerMode,
+          onChanged: (value) => setState(() {
+            developerMode = !developerMode;
+          }),
+        ),
+      ],
+    );
   }
 }
