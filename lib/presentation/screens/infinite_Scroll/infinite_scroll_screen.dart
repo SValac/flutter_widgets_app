fec95407d77e38c567f3751859175efc14e539da
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-16 16:25:22 -0600

    feat: convert InfiniteScrollScreen to StatefulWidget and implement image loading

--- lib/presentation/screens/infinite_Scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_Scroll/infinite_scroll_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const name = 'infinite_scroll_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Infinite Scroll & Pull to Refresh')),
+      body: ListView.builder(
+        itemCount: imagesIds.length,
+        itemBuilder: (context, index) {
+          return FadeInImage(
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/images/jar-loading.gif'),
+            image: NetworkImage(
+              'https://picsum.photos/id/${imagesIds[index]}/500/300',
+            ),
+          );
+        },
+      ),
+    );
   }
 }
