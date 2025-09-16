02952b5efd24448b6d7932e6ecc47c8452080ad4
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-16 16:04:06 -0600

    feat: convert AppTutorialScreen to StatefulWidget and implement page view controller with end reached logic

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -32,10 +32,54 @@ final slides = <SlidesInfo>[
   ),
 ];
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const name = 'app_tutorial_screen';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final pageViewController = PageController();
+
+  bool endReached = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    pageViewController.addListener(() {
+      /* 
+        at some point the controller may not be asigned to any page view,
+        so the page property will be null, to avoid errors we use the null coalescing
+        operator to assign a default value of 0 in that case.
+      
+       */
+      final page = pageViewController.page ?? 0;
+      print('${pageViewController.page}');
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          // print('End reached');
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  /* 
+    Always dispose controllers when the widget is removed from the tree
+    to avoid memory leaks.  
+   */
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    pageViewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +88,7 @@ class AppTutorialScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            controller: pageViewController,
             physics: BouncingScrollPhysics(),
             children: slides
                 .map(
@@ -63,6 +108,16 @@ class AppTutorialScreen extends StatelessWidget {
               child: const Text('Salir'),
             ),
           ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 50,
+                  child: FilledButton(
+                    onPressed: () => context.pop(),
+                    child: Text('Comenzar'),
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
